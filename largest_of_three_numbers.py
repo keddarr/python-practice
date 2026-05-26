cd360af3826467a20dcaf63c309fc0e896be8712
Kedar <kedarsreedhar3107@gmail.com>
2026-05-26 09:47:50 +0530

    Add script to find the largest of three numbers

---
+++ largest_of_three_numbers.py
@@ -0,0 +1,7 @@
+print("*** Check which among the Three numbers are the largest ***")
+a = int(input("Enter the 1st number : "))
+b = int(input("Enter the 2nd number : "))
+c = int(input("Enter the 3rd number : "))
+
+largest = max(a,b,c)
+print(f"{largest} is the largest number")
