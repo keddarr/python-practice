df9bc84802b04c94f39ba05b769d41660b68e67a
Kedar <kedarsreedhar3107@gmail.com>
2026-06-17 00:42:33 +0530

    Add transaction history feature to ATM system

--- atm_banking_system.py
+++ atm_banking_system.py
@@ -1,5 +1,10 @@
 balance = 10000
 atmpin = 8686
+history = []
+def showbalance():
+    s = input("Do you want to view your balance? (YES/NO)")
+    if s.lower() =='yes':
+        print(f"Remaining balance : {balance}")
 def deposit():
     global balance
     print("You have chose to deposit !")
@@ -21,18 +26,23 @@ def deposit():
     else:
         if acc == "886730010731":
             balance += cash
-            print(f"You have successfully deposited {cash} to Kedar")
-            print(f"Remaining balance : {balance}")
-
+            print(f"You have successfully deposited {cash} to yourself")
+            history.append(f"Deposited {cash}/- ")
+            showbalance()
         else:
             balance -= cash
-            print(f"You have successfully deposited {cash}")
-            print(f"Remaining balance : {balance}")
+            print(f"You have successfully deposited {cash} to {name}")
+            history.append(f"Transferred {cash}/- to {name}")
+            showbalance()
 def withdraw():
     global balance
     print("You have chosen to withdraw!")
 
-    pin = int(input("Enter your pin : "))
+    try :
+        pin = int(input("Enter your pin : "))
+    except ValueError:
+        print("PIN must be number only")
+        return
     i = int(input("How much cash do you wish to withdraw : "))
 
     if i > balance:
@@ -46,6 +56,8 @@ def withdraw():
             balance -= i
             print(f"You have successfully withdrawn: {i}.")
             print(f"Remaining balance : {balance}")
+            history.append(f"Withdrew {i}/-")
+            
 
         else:
             print("Invalid pin")
@@ -59,12 +71,21 @@ def checkbalance() :
 
     else:
         print("Invalid pin")
+def transactionhistory():
+    if len(history) == 0:
+        print("No history found")
+    else:
+        print("Transaction History : ")
+        for i in history :
+            print(i)
 while True:
+    print("====== ATM SYSTEM ======")
     print("What do you want to proceed with?")
     print("1.DEPOSIT")
     print("2.WITHDRAW")
     print("3.CHECK BALANCE")
-    print("4.EXIT")
+    print("4.Transaction History")
+    print("5.EXIT")
 
     a = input("Enter your choice : ")
 
@@ -75,13 +96,12 @@ while True:
     elif a == "3":
         checkbalance()
     elif a == "4":
+        transactionhistory()
+    elif a == "5":
         ask = input("Are you sure you want to exit?\n")
         if ask.lower() == "yes":
             break
         elif ask.lower() == "no":
-            print("You will be redirected to the MENU.")
-
-    
-    
+            print("You will now be redirected to the MENU.")
     else:
-        print("Invalid Choice, Enter (1 - 4)\n")
+        print("Invalid Choice, Enter (1 - 5)\n")
